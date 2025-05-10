ee608caa296d8218a1d1f44653c58cc51fe8b995
mmxdxmm <1658011573@qq.com>
2025-05-10 10:58:21 +0800

    Update build_setup.sh

--- build_setup.sh
+++ build_setup.sh
@@ -2,6 +2,9 @@
 # Assume building on ubuntu 24.04
 sudo apt update
 
+#install llvm-20
+wget https://apt.llvm.org/llvm.sh -O llvm.sh && chmod +x llvm.sh && sudo ./llvm.sh 20
+
 # Install Dependencies
 sudo apt -y install uuid-dev clang llvm gcc-aarch64-linux-gnu lld pip git mono-devel build-essential iasl nasm python3 python3-git python3-pip gettext locales gnupg ca-certificates python3-venv git-core curl
 
@@ -12,4 +15,4 @@ alias nuget="mono /usr/local/bin/nuget.exe"
 #cargo install --force cargo-make
 #cargo add cargo-tarpaulin
 
-export CLANGPDB_BIN=/usr/lib/llvm-38/bin/ && export CLANGPDB_AARCH64_PREFIX=aarch64-linux-gnu-
+export CLANGPDB_BIN=/usr/lib/llvm-20/bin && export CLANGPDB_AARCH64_PREFIX=aarch64-linux-gnu-
